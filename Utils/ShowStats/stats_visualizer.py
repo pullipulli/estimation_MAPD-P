5ffe65a4be74033dfd9aa384080551fcda1094f3
andrea.pullia <andrea.pullia@studenti.unimi.it>
2024-06-24 13:36:35 +0200

    Real vs estimated avg costs bar charts

--- Utils/ShowStats/stats_visualizer.py
+++ Utils/ShowStats/stats_visualizer.py
@@ -2,10 +2,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-METRIC_NAMES = ["costs", "serv_times", "pickup_to_goal_times", "start_to_pickup_times", "runtimes", "makespans"]
-METRIC_LABELS = ["Costs per Task", "Service Times", "Pickup to Goal Times", "Start to Pickup Times", "Runtimes", "Makespans"]
+TIME_METRIC_NAMES = ["costs", "serv_times", "pickup_to_goal_times", "start_to_pickup_times", "runtimes", "makespans"]
+TIME_METRIC_LABELS = ["Costs per Task", "Service Times", "Pickup to Goal Times", "Start to Pickup Times", "Runtimes",
+                      "Makespans"]
 
-METRICS = dict(zip(METRIC_NAMES, METRIC_LABELS))
+TIME_METRICS = dict(zip(TIME_METRIC_NAMES, TIME_METRIC_LABELS))
+
 
 class StatsVisualizer:
     def __init__(self, maps, agents_num, tasks_num, task_frequency_num, pickup_num, goal_num):
@@ -90,7 +92,7 @@ class StatsVisualizer:
                 run_ids.append(map["run_id"])
         return run_ids
 
-    def show_double_bar_metric(self, map_name, metric_name, metric_label):
+    def show_double_bar_time_metric(self, map_name, metric_name, metric_label):
         run_ids = self.get_run_ids_from_map(map_name)
         fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(5, 6))
 
@@ -140,3 +142,39 @@ class StatsVisualizer:
                    possible_variable_num)
         plt.legend()
         plt.show()
+
+    def show_real_vs_estimated_avg_costs(self, map_name):
+        run_ids = self.get_run_ids_from_map(map_name)
+
+        barWidth = 0.25
+
+        for run_id in run_ids:
+            fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(6, 6))
+            config, df_fixed, df_learning, df_fixed_costs, df_learning_costs = self.stats_of(run_id=run_id)
+            fixed_estimated_avg = np.mean(df_fixed_costs["estimated"])
+            learning_estimated_avg = np.mean(df_learning_costs["estimated"])
+            fixed_real_avg = np.mean(df_fixed_costs["real"])
+            learning_real_avg = np.mean(df_learning_costs["real"])
+
+            bar0 = np.arange(2)  # fixed estimated/real
+            bar1 = [x + barWidth for x in bar0]  # learning estimated/real
+
+            ax.bar(bar0, [fixed_estimated_avg, fixed_real_avg], color='r', width=barWidth,
+                   edgecolor='grey', label='Fixed')
+            ax.bar(bar1, [learning_estimated_avg, learning_real_avg], color='g', width=barWidth,
+                   edgecolor='grey', label='Learning')
+
+            for bars in ax.containers:
+                ax.bar_label(bars)
+
+            parameter_string = ""
+            for param in config:
+                if param != "map":
+                    parameter_string += f"{param}: {config[param]} "
+
+            plt.xlabel(f"Mappa: {config["map"]}\n" + parameter_string, fontweight='bold', fontsize=8)
+            plt.ylabel("Average Cost", fontweight='bold', fontsize=8)
+            plt.xticks([r + barWidth / 2 for r in range(2)],
+                       ["Estimated", "Real"])
+            plt.legend()
+            plt.show()
